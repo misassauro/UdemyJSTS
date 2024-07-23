4f6b9540423ebefbb8ec260bc2eeb44dcbdce56c
zMSLz <misaelg.macedo@gmail.com>
2024-07-23 16:54:45 -0300

    :sparkles: aula 13 + exercício

---
+++ JS/aula13/aula13.js
@@ -0,0 +1,9 @@
+//  0123456789
+let nome = "O rato roeu a roupa do rei de Roma";
+
+console.log(nome.lastIndexOf("M", 5));
+console.log(nome.match(/[a-z]/g));
+console.log(nome.search(/M/));
+console.log(nome.replace(/r/g, "p"));
+console.log(nome.slice(2, 6));
+console.log(nome.split(" "));
