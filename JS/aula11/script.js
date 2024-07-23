d3334fc7e362e7d9709e3ec0f62eb94d1fdbd314
zMSLz <misaelg.macedo@gmail.com>
2024-07-23 16:53:42 -0300

    :sparkles: aula 11

---
+++ JS/aula11/script.js
@@ -0,0 +1,8 @@
+num1 = prompt("Digite um número: ");
+num2 = prompt("Digite outro número");
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+const resultado = num1 + num2;
+alert(`O resultado é ${resultado}`);
