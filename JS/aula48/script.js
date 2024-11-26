b6e00f9f987117f9000506b13adf7d3280d36524
misassauro <misaelg.macedo@gmail.com>
2024-11-26 19:24:58 -0300

    differences between var and let

---
+++ JS/aula48/script.js
@@ -0,0 +1,45 @@
+// let nome = 'Misael'; //criando
+// var nome2 = 'Misa';
+
+// let nome = 'Misalel'; // uma variável do tipo let não pode ser redeclarada
+
+// var nome2 = 'Misinha'; // uma variável do tipo var pode ser redeclarada
+
+// Let tem escopo de bloco
+// Var só tem escopo de função
+// const verdadeira = true;
+
+// if (verdadeira) {
+//     let nome = 'Gomes'; // criando
+//     var nome2 = 'Adrian';
+
+//     // console.log(nome, nome2);
+
+//     if (verdadeira) {
+//         let nome = 'Outra coisa';  // criando
+//         var nome2 = 'Manhães';
+//         // console.log(nome, nome2);
+
+//         // Se a variável 'nome' for encontrada dentro deste bloco IF, o seu valor será impresso no console.log(). Caso não, ele busca por uma variável 'nome' no bloco anterior e assim sucessivamente.
+//     }
+
+//     console.log(nome2);
+// }
+const verdadeira = true;
+
+// function falaOi() {
+//     var sobrenome = 'Miranda';
+
+//     if (verdadeira) {
+//         let nome = 'Luiz';
+//         console.log(sobrenome);
+//     }
+
+//     console.log(nome); // a variável 'nome' não pode ser acessada aqui.
+// }
+
+// falaOi();
+
+console.log(sobrenome); // retorna "undefined"
+
+var sobrenome = 'Misa';
